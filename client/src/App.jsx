02640a9fb39613a702f8eabdf61881de9ed02167
tftayyab <tftayyabfaisal@gmail.com>
2025-06-18 01:29:58 +0500

    Css Tailwind is working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,9 @@
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div>
-      <Helmet>
-        <title>Task Manager</title>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet" />
-      </Helmet>
 
-      <h1>Welcome to Task Manager</h1>
-      <nav>
-        <ul>
-          <li><Link to="/tasks">View All Tasks</Link></li>
-        </ul>
-      </nav>
-    </div>
+  return (
+    <Home />
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import '../App.css'
+
+function Home() {
+
+  return (
+    <>
+
+    <head>
+        <title>Home</title>
+    </head>
+
+     <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
+        Tailwind is Working!
+    </h1>
+    </>
+  );
+}
+
+export default Home;
